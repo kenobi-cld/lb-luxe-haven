2051da2c71f7297cd8c4a70fd5c9440461e9f360
kenobi-sc <whothekenobi@gmail.com>
2026-01-27 18:58:21 +0530

    1

--- src/scripts/initProjects.js
+++
@@ -1,8 +0,0 @@
-import "./scrollReveal.js";
-import "./textReveal.js";
-import "./filterProjects.js";
-import initProjectModal from "./modalOpen.js";
-import initCarousel from "../scripts/carousel.js";
-
-initCarousel(".embla__viewport");
-initProjectModal();
---
+++ src/scripts/main.js
@@ -0,0 +1,12 @@
+import "./scrollReveal.js";
+import "./textReveal.js";
+import "./filterProjects.js";
+import initProjectModal from "./modalOpen.js";
+import "./filterProjects.js";
+import "./heroIntro.js";
+import "./modalOpen.js";
+import "./scrollReveal.js";
+import "./testimonialCarousel.js";
+import "./textReveal.js";
+
+initProjectModal();
