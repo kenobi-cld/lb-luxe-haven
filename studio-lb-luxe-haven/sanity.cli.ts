f9e98eec47713a1e12c9c4894537a5c23b58b66b
kenobi-sc <whothekenobi@gmail.com>
2026-02-01 13:41:42 +0530

    jadsajdba

--- studio-lb-luxe-haven/sanity.cli.ts
+++ studio-lb-luxe-haven/sanity.cli.ts
@@ -10,6 +10,7 @@ export default defineCliConfig({
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
+     appId: 'ozjejxkc20go9dip1squ12j3',
     autoUpdates: true,
   }
 })
